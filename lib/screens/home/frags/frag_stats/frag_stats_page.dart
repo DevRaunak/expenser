629a8e44a549a5d1c3ed2bc15ea976b041b9dd5c
raunakpurohit <raunak.wscubetech@gmail.com>
2023-04-27 13:48:34 +0530

    Month wise Graph Integration

--- lib/screens/home/frags/frag_stats/frag_stats_page.dart
+++ lib/screens/home/frags/frag_stats/frag_stats_page.dart
@@ -1,7 +1,9 @@
+import 'package:expenser/models/cat_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+import '../../../../bloc/ExpenseCatType/expense_type_bloc.dart';
 import '../../../../bloc/expense_bloc.dart';
 import '../../../../constants.dart';
 import '../../../../models/expense_model.dart';
@@ -16,20 +18,25 @@ class FragStatsPage extends StatefulWidget {
 
 class _FragStatsPageState extends State<FragStatsPage> {
   List<ExpenseModel> arrExpense = [];
+  List<CatModel> arrExpenseType = [];
   List<Map<String, dynamic>> arrMonthWiseBal = [];
+  List<Map<String, dynamic>> arrTypeWiseBal = [];
 
   @override
   void initState() {
     super.initState();
 
     BlocProvider.of<ExpenseBloc>(context).add(FetchExpenseEvent());
+    BlocProvider.of<ExpenseTypeBloc>(context).add(FetchAllCatExpenseType());
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
+        backgroundColor: Theme
+            .of(context)
+            .backgroundColor,
         body: BlocBuilder<ExpenseBloc, ExpenseState>(
           builder: (context, state) {
             if (state is ExpenseLoadingState) {
@@ -46,7 +53,9 @@ class _FragStatsPageState extends State<FragStatsPage> {
                   var date = DateTime.parse(expense.time!);
 
                   var eachMonth =
-                      '${date.year}-${date.month.toString().length == 1 ? '0${date.month}' : '${date.month}'}';
+                      '${date.year}-${date.month
+                      .toString()
+                      .length == 1 ? '0${date.month}' : '${date.month}'}';
 
                   if (!arrUniqueMonths.contains(eachMonth)) {
                     arrUniqueMonths.add(eachMonth);
@@ -57,40 +66,94 @@ class _FragStatsPageState extends State<FragStatsPage> {
 
                 filterExpenseMonthWise(arrUniqueMonths);
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Month',
-                        style: TextStyle(fontSize: 34, color: MyColor.bgWColor),
-                      ),
-                      SizedBox(
-                        height: 21,
-                      ),
-                      /*arrMonthWiseBal.isNotEmpty ? SfCartesianChart(
-                       primaryXAxis: CategoryAxis(),
-                       series: <ChartSeries<Map<String, dynamic>, String>>[
-                         ColumnSeries(
-                             dataSource: arrMonthWiseBal,
-                             xValueMapper: (Map<String, dynamic> data, _)=> data['month'],
-                             yValueMapper: (Map<String, dynamic> data, _)=> data['bal'])
-                       ],
-                     ) : Container()*/
 
-                     /* SfCartesianChart(
-                        primaryXAxis: CategoryAxis(),
-                        series: <LineSeries<Map<String, dynamic>, String>>[
-                          LineSeries<Map<String, dynamic>, String>(
-                              dataSource: arrMonthWiseBal,
-                              xValueMapper: (Map<String, dynamic> data, _) =>
-                                  data['month'],
-                              yValueMapper: (Map<String, dynamic> data, _) =>
-                                  data['bal'])
-                        ],
-                      )*/
-                    ],
-                  ),
+                return BlocBuilder<ExpenseTypeBloc, ExpenseTypeState>(
+                  builder: (context, state) {
+                    if(state is ExpenseTypeLoadingState){
+                      return CircularProgressIndicator();
+                    } else if(state is ExpenseTypeLoadedState) {
+                      arrExpenseType = state.arrExpenseType;
+                      filterExpenseTypeWise();
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Text(
+                                'Month',
+                                style: TextStyle(
+                                    fontSize: 34, color: MyColor.bgWColor),
+                              ),
+                              SizedBox(
+                                height: 21,
+                              ),
+                              arrMonthWiseBal.isNotEmpty ? SfCartesianChart(
+                                primaryXAxis: CategoryAxis(),
+
+                                series: <
+                                    ChartSeries<Map<String, dynamic>, String>>[
+                                  ColumnSeries(
+                                      dataSource: arrMonthWiseBal,
+                                      xValueMapper: (Map<String, dynamic> data,
+                                          _) => data['month'],
+                                      yValueMapper: (Map<String, dynamic> data,
+                                          _) => data['bal'])
+                                ],
+                              ) : Container(),
+                              SizedBox(
+                                height: 11,
+                              ),
+                              Text(
+                                'Type',
+                                style: TextStyle(
+                                    fontSize: 34, color: MyColor.bgWColor),
+                              ),
+                              SizedBox(
+                                height: 11,
+                              ),
+                              SfCartesianChart(
+                                primaryXAxis: CategoryAxis(
+                                  autoScrollingMode: AutoScrollingMode.start,
+                                  title: AxisTitle(
+                                    text: 'Expense Category'
+                                  ),
+                                ),
+                                primaryYAxis: NumericAxis(minimum: 0, maximum: 5000, interval: 2000),
+                                series: <
+                                    ChartSeries<Map<String, dynamic>, String>>[
+                                  ColumnSeries<Map<String, dynamic>, String>(
+                                    borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
+                                      dataSource: arrTypeWiseBal,
+                                      xValueMapper: (Map<String, dynamic> data,
+                                          _) =>
+                                      data['catName'],
+                                      yValueMapper: (Map<String, dynamic> data,
+                                          _) =>
+                                      data['bal'])
+                                ],
+                              ),
+
+                              SfCartesianChart(
+                                primaryXAxis: CategoryAxis(
+                                  title: AxisTitle(
+                                      text: 'Expense Category'
+                                  ),
+                                ),
+                                primaryYAxis: NumericAxis(minimum: 0, maximum: 5000, interval: 1000),
+                                series: <ChartSeries<Map<String, dynamic>, String>>[
+                                  AreaSeries<Map<String, dynamic>, String>(
+                                      dataSource: arrTypeWiseBal,
+                                      xValueMapper: (Map<String, dynamic> data, _)=> data['catName'],
+                                      yValueMapper: (Map<String, dynamic> data, _)=> data['bal'])
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                    return Container();
+                  },
                 );
               } else {
                 return Container(
@@ -150,4 +213,38 @@ class _FragStatsPageState extends State<FragStatsPage> {
 
     print(arrMonthWiseBal);
   }
+
+  void filterExpenseTypeWise() {
+    for (CatModel eachType in arrExpenseType) {
+      List<ExpenseModel> arrEachTypeExpense = [];
+
+      for (ExpenseModel expense in arrExpense) {
+        if (expense.catId==eachType.catId) {
+          arrEachTypeExpense.add(expense);
+        }
+      }
+
+      double eachTypeBal = 0.0;
+
+      for (ExpenseModel expense in arrEachTypeExpense) {
+        if (expense.expenseType == "Debit") {
+          eachTypeBal += expense.amt!;
+        } else {
+          eachTypeBal -= expense.amt!;
+        }
+      }
+
+
+      if(eachTypeBal!=0.0) {
+        arrTypeWiseBal.add({
+          'catName': eachType.name,
+          'bal': eachTypeBal
+        });
+      }
+    }
+
+    print(arrTypeWiseBal);
+  }
+
+
 }
